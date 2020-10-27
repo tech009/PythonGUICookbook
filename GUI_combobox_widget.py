f7a9c3eebd98e4e2a1a5214eac368e1d8ad3a4df
tech009 <in_aman_arya@outlook.com>
2020-10-27 19:18:14 +0530

    Add files via upload

---
+++ GUI_combobox_widget.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+
+win = tk.Tk()
+
+win.title('ComboBox_Widget')
+
+name = tk.StringVar()
+name_entered = ttk.Entry(win, width=12, textvariable=name)
+name_entered.grid(column=0, row=1)
+
+action = ttk.Button(win, text='Click Me!', command=win.quit)
+action.grid(column=2, row=1)
+
+ttk.Label(win, text='Choose a number:').grid(column=1, row=0)
+number = tk.StringVar()
+number_chosen = ttk.Combobox(win, width=12, textvariable=number)
+number_chosen['values'] = (1, 2, 4, 42, 100)
+number_chosen.grid(column=1, row=1)
+number_chosen.current(0)
+
+name_entered.focus()
+
+win.mainloop()
